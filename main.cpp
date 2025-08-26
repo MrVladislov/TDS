bb107272dc76948a2c60ea07ae036907cfd0e7a4
MrVladislov <mrvladislov67@gmail.com>
2025-08-26 18:42:42 +0200

    stage 1

--- main.cpp
+++ main.cpp
@@ -1,38 +1,14 @@
+//g++ main.cpp -o game.exe -I"D:\Vlad\SFML-3.0.0-windows-gcc-14.2.0-mingw-64-bit\SFML-3.0.0\include" -L"D:\Vlad\SFML-3.0.0-windows-gcc-14.2.0-mingw-64-bit\SFML-3.0.0\lib" -lsfml-graphics -lsfml-window -lsfml-system
 #include <functional>
 #include <vector>
 #include <iostream>
 #include <string> 
 #include "visual.h"
+#include <SFML/Graphics.hpp>
 
 
 int main() {
-    sf::RenderWindow window(
-        sf::VideoMode(sf::Vector2u(1000, 600)),
-        "TD Game",
-        sf::Style::Titlebar | sf::Style::Close
-    );
 
-    sf::Font font;
-    if (!font.openFromFile("arialmt.ttf")) { // Убедитесь, что файл шрифта доступен
-        std::cerr << "Failed to load font!\n";
-        return 1;
-    }
-
-    GameUI ui(window, font);
-
-    while (window.isOpen()) {
-        while (auto event = window.pollEvent()) {
-            if (event->is<sf::Event::Closed>()) {
-                window.close();
-            }
-            ui.handleEvent(*event);
-        }
-
-        ui.update(150, 10);
-        window.clear(sf::Color(30, 30, 30));
-        ui.draw();
-        window.display();
-    }
 
     return 0;
 }
